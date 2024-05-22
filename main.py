8664aa83f03ace72e27044dfdb45e18a5d40ab01
Mannopboy <mannopboy8@gmail.com>
2024-05-22 13:18:50 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+a = 0
+
+# a = np.arange(20).reshape(4, 5)
+# print(a.shape)
+# (4, 5)
+# print(a.ndim)
+# 2
+# print(a.dtype)
+# int32
+# print(a.size)
+# 20
+
+# a = np.array([0, 1, 2, 3, 4, 5])
+# [0 1 2 3 4 5]
+
+# a = np.zeros((3, 4), dtype=np.int16)
+# [[0 0 0 0] [0 0 0 0] [0 0 0 0]]
+
+# a = np.arange(2, 30, 2)
+# [ 2  4  6  8 10 12 14 16 18 20 22 24 26 28]
+
+# a = np.linspace(0, 5, 10)
+# [0. 0.55555556 1.11111111 1.66666667 2.22222222 2.77777778 3.33333333 3.88888889 4.44444444 5.]
+
+# a = np.arange(6)
+# 1d array
+
+# a = np.arange(12).reshape(4, 3)
+# 2d array
+
+# a = np.arange(24).reshape(2, 3, 4)
+# 3d array
+
+# a = np.arange(100000)
+# [0  1  2 ... 99997 99998 99999]
+
+# a = np.array([20, 30, 40, 50])
+# [20 30 40 50]
+# b = np.arange(4)
+# [0 1 2 3]
+# c = a - b
+# [20, 29, 38, 47]
+
+# rg = np.random.default_rng(1)
+# a = rg.random((1, 3))
+# [[0.51182162 0.9504637  0.14415961]]
+
+print(a)
